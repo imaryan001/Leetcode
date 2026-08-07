c86d7afaacca2cac0d61c9b8819d713e4d108454
Rohit <rohitgorais143@gmail.com>
2026-08-07 14:14:13 +0530

    Add files via upload

---
+++ Leetcode_Q21_merge _two_sorted_list.py
@@ -0,0 +1,50 @@
+
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+        dummy = ListNode(-1)
+        current = dummy
+
+        while list1 and list2:
+
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+
+            else:
+                current.next = list2
+                list2 = list2.next
+
+            current = current.next
+
+
+        
+        if list1:
+            current.next = list1
+
+       
+        if list2:
+            current.next = list2
+
+
+        return dummy.next
+        
+list1 = ListNode(1, ListNode(2, ListNode(4)))
+list2 = ListNode(1, ListNode(3, ListNode(4)))
+
+obj = Solution()
+
+result = obj.mergeTwoLists(list1, list2)
+
+
+
+while result:
+    print(result.val, end=" ")
+    result = result.next
